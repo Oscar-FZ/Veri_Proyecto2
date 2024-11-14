0375012dd42d0b863efd66af9fb2b47d6aae5b98
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-14 17:46:00 -0600

    Step 1

--- scoreboard.sv
+++ scoreboard.sv
@@ -7,6 +7,10 @@ class scoreboard extends uvm_scoreboard;
 
     //TODO Definir variables que seran usadas para el checkeo
     shortreal exp_result;
+    // Variables para la simulacion
+    bit fp_X_sign;
+    bit fp_Y_sign;
+    bit result_sign;
 
     uvm_analysis_imp #(Item, scoreboard) m_analysis_imp;
 
@@ -20,23 +24,21 @@ class scoreboard extends uvm_scoreboard;
         shortreal fp_X_float = $bitstoshortreal(item.fp_X);
         shortreal fp_Y_float = $bitstoshortreal(item.fp_Y);
         shortreal fp_Z_float = $bitstoshortreal(item.fp_Z);
+
+        // Variables para simular la multiplicacion en punto flotante
+        fp_X_sign = item.fp_X[31];
+        fp_Y_sign = item.fp_Y[31];
         
 
-        `uvm_info("SCBD", $sformatf("r mode=%0d X=%e Y=%e Z=%e Overflow=%b Underflow=%b",
-            item.r_mode, fp_X_float, fp_Y_float, fp_Z_float, item.ovrf, item.udrf), UVM_LOW)
+        `uvm_info("SCBD", $sformatf("r mode=%0d X=%e Y=%e Z=%e Overflow=%b Underflow=%b", 
+        item.r_mode, fp_X_float, fp_Y_float, fp_Z_float, item.ovrf, item.udrf), UVM_LOW)
         
         //TODO Hacer checker
         
-        if (fp_Z_float != exp_result) begin
-            `uvm_error("SCBD", $sformatf("ERROR! fp_Z_float=%0e exp=%0e", fp_Z_float, exp_result))
-        end
-        
-        
-        
-        case(item.r_mode) 
-            3'b001: exp_result = fp_X_float * fp_Y_float;
-            default: exp_result = 'bx;
-        endcase
+        // 1. Determinar el signo
+        result_sign = fp_X_sign ^ fp_Y_sign;
+
+        `uvm_info("SCBD", $sformatf("result_sign=%b", result_sign), UVM_LOW)
 
     endfunction
 endclass
--- test.sv
+++ test.sv
@@ -50,7 +50,8 @@ class test_fpmul extends base_test;
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        seq.randomize() with {num inside {[12: 22]};};
+        //seq.randomize() with {num inside {[12: 22]};};
+        seq.randomize() with {num inside {1};};
     endfunction
 endclass
 
