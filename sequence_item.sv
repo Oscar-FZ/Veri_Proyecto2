c401df25199a7cd9a676c106b058b91958a6f225
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-16 00:25:34 -0600

    Try there

--- sequence_item.sv
+++ sequence_item.sv
@@ -27,6 +27,9 @@ class Item extends uvm_sequence_item;
     `uvm_object_utils_end*/
 
     virtual function string print();
+        if (test_s) begin
+            $display("Test still worked lmao");
+        end
         return $sformatf("fp_X = %h, fp_Y = %h, fp_Z = %h, R_mode = %h, Ovrf = %h, Udrf = %h", {sign_X, exp_X, frac_X}, {sign_Y, exp_Y, frac_Y}, fp_Z, r_mode, ovrf, udrf);
     endfunction
 
@@ -41,9 +44,7 @@ class Item extends uvm_sequence_item;
     //                (exp_X + exp_Y -127) > -126;
     //                solve exp_X before exp_Y;}
     //constraint c1 {(exp_X + exp_Y - 127) > 128;}
-    if (test_s) begin
-        $display("Test still worked lmao");
-    end
+    
 
     constraint c2 {r_mode inside{[0:4]};}
 endclass
