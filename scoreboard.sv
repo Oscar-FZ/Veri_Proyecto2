f93f27ecbc5bd80e5e946ec7c5c4ecce955ebb0b
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-17 08:35:56 -0600

    this probably wont work

--- scoreboard.sv
+++ scoreboard.sv
@@ -168,13 +168,13 @@ class scoreboard extends uvm_scoreboard;
             Z_aux = {sign_Z, 31'b0000_0000_0000_0000_0000_0000_0000_000};
         end
 
-        else if ((item.exp_X == 8'hFF) || (item.exp_Y == 8'hFF)) begin 
+        //else if ((item.exp_X == 8'hFF) || (item.exp_Y == 8'hFF)) begin 
             if ((item.exp_X == 8'hFF && item.frac_X != 0) || (item.exp_Y == 8'hFF && item.frac_Y != 0) || 
             (item.exp_Y == 8'hFF && item.frac_Y != 0 && item.exp_X == 8'h00 && item.frac_X == 23'h000000) || (item.exp_X == 8'hFF && item.frac_X != 0 && item.exp_Y == 8'h00 && item.frac_Y == 23'h000000)) begin // Multiplicacion por NaN
                 Z_aux = {1'b0, 31'b1111_1111_1000_0000_0000_0000_0000_000}; //El DUT solo genera +NaN
             end
 
-            else if if ((item.exp_X == 8'hFF && item.frac_X == 0) || (item.exp_Y == 8'hFF && item.frac_Y == 0)) begin //Multiplicacion por infinito
+            else if ((item.exp_X == 8'hFF && item.frac_X == 0) || (item.exp_Y == 8'hFF && item.frac_Y == 0)) begin //Multiplicacion por infinito
                 Z_aux = {sign_Z, 31'b1111_1111_0000_0000_0000_0000_0000_000};
             end
 
@@ -188,7 +188,8 @@ class scoreboard extends uvm_scoreboard;
                 //Z_aux = {1'b0, 31'b1111_1111_1000_0000_0000_0000_0000_000}; //El DUT solo genera +NaN
                 //$display("Estamos en mul NaN mi gente");
             //end
-        end
+            
+        //end
 
         // Separar esto en otro if/else
         if (exp_X+exp_Y >= 382) begin //Overflow 
