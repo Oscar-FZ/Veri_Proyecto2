202224ab972f05b28d2b376fef211dc81e92c46f
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-17 14:53:29 -0600

    Debugging

--- scoreboard.sv
+++ scoreboard.sv
@@ -170,12 +170,14 @@ class scoreboard extends uvm_scoreboard;
 
         else if ((item.exp_X == 8'hFF) || (item.exp_Y == 8'hFF)) begin 
             if ((item.exp_X == 8'hFF && item.frac_X != 0) || (item.exp_Y == 8'hFF && item.frac_Y != 0) || 
-            (item.exp_Y == 8'hFF && item.frac_Y != 0 && item.exp_X == 8'h00 && item.frac_X == 23'h000000) || (item.exp_X == 8'hFF && item.frac_X != 0 && item.exp_Y == 8'h00 && item.frac_Y == 23'h000000)) begin // Multiplicacion por NaN
+            (item.exp_Y == 8'hFF && item.frac_Y != 0 && item.exp_X == 8'h00 && item.frac_X == 23'h000000) || 
+            (item.exp_X == 8'hFF && item.frac_X != 0 && item.exp_Y == 8'h00 && item.frac_Y == 23'h000000)) begin // Multiplicacion por NaN
                 Z_aux = {1'b0, 31'b1111_1111_1000_0000_0000_0000_0000_000}; //El DUT solo genera +NaN
             end
 
             else begin //Multiplicacion por infinito
                 Z_aux = {sign_Z, 31'b1111_1111_0000_0000_0000_0000_0000_000};
+                $display("Mala noticia mi gente");
             end
         end
 
