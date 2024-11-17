fa19aa1e0f398ef748f9b49c7c58967d8813d9c9
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-17 02:49:29 -0600

    aver

--- scoreboard.sv
+++ scoreboard.sv
@@ -170,11 +170,15 @@ class scoreboard extends uvm_scoreboard;
         end
 
         else if ((item.exp_X == 8'hFF) || (item.exp_Y == 8'hFF)) begin 
-            if (((item.frac_X == 23'h000000) || (item.frac_Y == 23'h000000)) && ((item.frac_X[22] != 1'b1) || (item.frac_Y[22] != 1'b1))) begin //Multiplicacion por infinito
-                Z_aux = {sign_Z, 31'b1111_1111_0000_0000_0000_0000_0000_000};
-                $display("Estamos en mul infinito mi gente");
-                $display("fracx:%0b, fracy:%0b", item.frac_X, item.frac_Y);
+            if ((item.frac_X[22] == 1'b1) || (item.frac_Y[22] == 1'b1)) begin
+                $display("A ver si funciona")
             end
+
+            //if (((item.frac_X == 23'h000000) || (item.frac_Y == 23'h000000)) && ((item.frac_X[22] != 1'b1) || (item.frac_Y[22] != 1'b1))) begin //Multiplicacion por infinito
+                //Z_aux = {sign_Z, 31'b1111_1111_0000_0000_0000_0000_0000_000};
+                //$display("Estamos en mul infinito mi gente");
+                //$display("fracx:%0b, fracy:%0b", item.frac_X, item.frac_Y);
+            //end
 
             else if (((item.frac_X != 23'h000000) || (item.frac_Y != 23'h000000)) && ((item.frac_X[22] == 1'b1) || (item.frac_Y[22] == 1'b1))) begin //Multiplicacion por NaN
                 Z_aux = {1'b0, 31'b1111_1111_1000_0000_0000_0000_0000_000}; //El DUT solo genera +NaN
