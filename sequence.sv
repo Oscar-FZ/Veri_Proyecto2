7e9fd099a2feb24c86369212c784143c84163711
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-16 20:17:35 -0600

    Increased number of transactions

--- sequence.sv
+++ sequence.sv
@@ -10,7 +10,7 @@ class gen_item_seq extends uvm_sequence;
     rand int num;
     rand int retardo_envio;
 
-    constraint c1 {soft num inside {[10:50]};}
+    constraint c1 {soft num inside {[50:100]};}
     //constraint c2 {soft retardo_envio inside {[100:300]};}
 
     virtual task body();
