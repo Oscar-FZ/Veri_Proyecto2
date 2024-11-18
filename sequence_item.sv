517b0dab4972dfcc81e73b1f56a7483374602add
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-17 23:12:46 -0600

    removed commented line

--- sequence_item.sv
+++ sequence_item.sv
@@ -6,7 +6,6 @@ class Item extends uvm_sequence_item;
     rand bit sign_X, sign_Y;
     rand bit [7:0] exp_X, exp_Y;
     rand bit [22:0] frac_X, frac_Y;
-    //bit [31:0] fp_X, fp_Y;
 
     bit test_s;
     bit c_ident;
@@ -16,21 +15,19 @@ class Item extends uvm_sequence_item;
     bit c_ovrf;
     bit c_udrf;
 
-    //fp_X = {sign_X, exp_X, frac_X};
-    //fp_Y = {sign_Y, exp_Y, frac_Y};
     //Outputs
     bit [31:0] fp_Z;
     bit ovrf, udrf;
 
     //Registro de los atributos para usar las funciones de los macros de campo
-    /*`uvm_object_utils_begin(Item)
+    `uvm_object_utils_begin(Item)
         `uvm_field_int (r_mode, UVM_DEFAULT)
         `uvm_field_int (fp_X, UVM_DEFAULT)
         `uvm_field_int (fp_Y, UVM_DEFAULT)
         `uvm_field_int (fp_Z, UVM_DEFAULT)
         `uvm_field_int (ovrf, UVM_DEFAULT)
         `uvm_field_int (udrf, UVM_DEFAULT)
-    `uvm_object_utils_end*/
+    `uvm_object_utils_end
 
     virtual function string print();
         return $sformatf("fp_X = %h, fp_Y = %h, fp_Z = %h, R_mode = %h, Ovrf = %h, Udrf = %h", {sign_X, exp_X, frac_X}, {sign_Y, exp_Y, frac_Y}, fp_Z, r_mode, ovrf, udrf);
@@ -40,15 +37,7 @@ class Item extends uvm_sequence_item;
     function new(string name = "Item");
         super.new(name);
     endfunction
-
-    //Constraints
-    //TODO No se como aleatorizar datos punto flotante xd
-    //constraint c1 {(exp_X + exp_Y - 127) < 128;
-    //                (exp_X + exp_Y -127) > -126;
-    //                solve exp_X before exp_Y;}
-    //constraint c1 {(exp_X + exp_Y - 127) > 128;}
     
-
     constraint c2 {r_mode inside{[0:4]};}
 
     constraint test_ident {
@@ -97,4 +86,5 @@ class Item extends uvm_sequence_item;
             exp_Y <= 8'b00111111;
         }
     }
+    
 endclass
