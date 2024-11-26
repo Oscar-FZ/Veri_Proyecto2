070388f5c75fbdcef8467f8f072901b693f0dde7
HalfS0ur <riveras.kendall99@gmail.com>
2024-11-26 00:12:55 -0600

    removed comments

--- sequence_item.sv
+++ sequence_item.sv
@@ -57,10 +57,8 @@ class Item extends uvm_sequence_item;
     constraint test_cero { 
         if (c_cero) {
             exp_Y dist {8'b00000000 := 50, [8'b0000_0001:8'b1111_1110] :/ 50};
-            //frac_Y dist {23'b0 := 50, [23'h1:23'h7fffff] :/ 50};
 
             exp_X dist {8'b00000000 := 50, [8'b0000_0001:8'b1111_1110] :/ 50};
-            //frac_X dist {23'b0 := 50, [23'h1:23'h7fffff] :/ 50};
 
             if (exp_Y == 8'b00000000) {
                 frac_Y == 23'b0;
@@ -78,10 +76,8 @@ class Item extends uvm_sequence_item;
     constraint test_nxi { 
         if (c_nxi) {
             exp_X dist {8'hFF := 50, [8'b0000_0000:8'b1111_1110] :/ 50};
-            //frac_X dist {23'b0 := 50, [23'h1:23'h7fffff] :/ 50};
 
             exp_Y dist {8'hFF := 50, [8'b0000_0000:8'b1111_1110] :/ 50};
-            //frac_Y dist {23'b0 := 50, [23'h1:23'h7fffff] :/ 50};
 
             if (exp_X == 8'hFF) {
                 frac_X == 23'b0;
